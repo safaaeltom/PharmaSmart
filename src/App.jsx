4cd0de079d5ae0164ba6d74783e56b6a952893f1
Safaa <safaaeltom@gmail.com>
2026-07-08 13:09:13 +0300

    feat: add dashboard page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 import Inventory from './pages/Inventory';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div>
       <Routes>
         <Route path="/inventory" element={<Inventory />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
-    </div>
   );
 }
 
