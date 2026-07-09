aec8f3b0222ceeb889498a752e89f59505595ecd
Safaa <safaaeltom@gmail.com>
2026-07-09 22:19:43 +0300

    feat: render medicine list dynamically with map

--- src/components/Inventory/MedicineList.jsx
+++ src/components/Inventory/MedicineList.jsx
@@ -1,7 +1,25 @@
 const MedicineList = () => {
+    const medicines = [
+        { id: 1, name: "Paracetamol", quantity: 24 },
+        { id: 2, name: "Ibuprofen", quantity: 5 },
+        { id: 3, name: "Atenolol", quantity: 17 },
+        { id: 4, name: "Amoxicillin", quantity: 37 }
+    ];
+
     return ( 
-        <div>
-            <h2>Medicine List</h2>
+        <div id="medicines-list">
+            <h2>Available medicines</h2>
+
+            <ul>
+                {medicines.map((medicine)=>(
+                    <li key={medicine.id}>
+                        <span className="name">{medicine.name}</span>
+                        <span className="quantity">{medicine.quantity}</span>
+                        <span className="delete">Delete</span>
+                        <span className="edit">Edit</span>
+                    </li>
+                ))}
+            </ul>
         </div>
      );
 }
