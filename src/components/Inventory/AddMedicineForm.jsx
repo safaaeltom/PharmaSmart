fdec9272cc5c9ec3d8935f8eb7d64722badfedb1
Safaa <safaaeltom@gmail.com>
2026-07-10 13:21:59 +0300

    feat: implement add medicine form UI

--- src/components/Inventory/AddMedicineForm.jsx
+++ src/components/Inventory/AddMedicineForm.jsx
@@ -1,8 +1,12 @@
 const AddMedicineForm = () => {
     return ( 
-        <div>
-            <h2>Add Medicine Form</h2>
-        </div>
+         <form id="add-medicine">
+            <label htmlFor="medicine-name">Medicine Name</label>
+            <input id="medicine-name" type="text" placeholder="Enter medicine name"/>
+            <label htmlFor="medicine-quantity">Quantity</label>
+            <input id="medicine-quantity" type="number" placeholder="Enter quantity"/>
+            <button type="submit">Add Medicine</button>
+        </form>
      );
 }
  
